3088e0c4efc2ab3eb4a0bad466331b38845736bf
MatVira <mviracucha14@gmail.com>
2025-07-15 11:18:44 -0500

    Quinto commit: Se agregó connectedCallback para inicializar la interfaz y capturar eventos

--- proyecto-par-impar/components/input-range.js
+++ proyecto-par-impar/components/input-range.js
@@ -4,6 +4,29 @@ class InputRange extends HTMLElement {
     this.attachShadow({ mode: 'open' });
   }
 
+  connectedCallback() {
+    this.shadowRoot.innerHTML = `
+
+      <style>
+
+        label, input, button {
+          margin: 5px;
+          font-family: Arial;
+        }
+
+      </style>
+
+      <label>Inicio: <input type="number" id="inicio" /></label>
+      <br>
+      <label>Fin: <input type="number" id="fin" /></label>
+      <br>
+      <br>
+      <button id="enviar">Mostrar</button>
+
+    `;
+
+    this.shadowRoot.querySelector('#enviar').addEventListener('click', () => this.enviarRango());
+  }
 
 
 }
