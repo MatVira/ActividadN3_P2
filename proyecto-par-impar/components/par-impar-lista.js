4d46a98be69f4337b7ceb6bfb0ab11cee5bb07e3
MatVira <mviracucha14@gmail.com>
2025-07-15 14:21:20 -0500

    Noveno commit: Se implementó connectedCallback en par-impar-lista para renderizar la interfaz y escuchar el evento rango-seleccionado

--- proyecto-par-impar/components/par-impar-lista.js
+++ proyecto-par-impar/components/par-impar-lista.js
@@ -4,6 +4,37 @@ class ParImparLista extends HTMLElement {
     this.attachShadow({ mode: 'open' });
   }
 
+  connectedCallback() {
+    this.shadowRoot.innerHTML = `
+
+      <style>
+
+        ul {
+
+          font-family: Arial;
+          padding-left: 20px;
+
+        }
+
+        li {
+          margin: 3px 0;
+        }
+
+      </style>
+
+      <h3>Resultado:</h3>
+      <ul id="lista"></ul>
+
+    `;
+
+    window.addEventListener('rango-seleccionado', (e) => {
+      const { inicio, fin } = e.detail;
+      this.mostrarLista(inicio, fin);
+    });
+  }
+
+  
+
 }
 
 customElements.define('par-impar-lista', ParImparLista);
